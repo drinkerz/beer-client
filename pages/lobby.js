92a8149aec950de18a31900ff027deb4d01183f3
gandalf <who7117@outlook.kr>
2019-08-01 18:07:16 +0900

    Update Lobby page

--- pages/lobby.js
+++ pages/lobby.js
@@ -26,6 +26,7 @@ const Body = styled.div`
   height: 100%;
   z-index: 2;
   display: flex;
+  flex-direction: column;
 `;
 
 const Header = styled.div`
@@ -79,6 +80,59 @@ const MenuButton = styled.a`
   }
 `;
 
+const Contents = styled.div`
+  width: 100%;
+  min-height: 860px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  z-index: 2;
+`;
+
+const Card = styled.div`
+  width: 180px;
+  height: 240px;
+  border-radius: 7px;
+  padding: 5px;
+  background-color: #000;
+  cursor: pointer;
+  border: 1px solid white;
+  margin: 1em;
+
+  &:hover img {
+    transform: rotate(15deg);
+  }
+`;
+
+const CardImage = styled.div`
+  height: 70%;
+  text-align: center;
+  background-image: url('/static/images/bg/beer_bg.jpg');
+  background-size: cover;
+  border-radius: 5px;
+
+  img {
+    height: 75%;
+    margin-top: 20%;
+  }
+`;
+
+const CardText = styled.div`
+  height: 30%;
+  color: white;
+  padding: 10px;
+
+  .beer_name {
+    border-bottom: 1px solid #fff;
+    margin-bottom: 5px;
+  }
+
+  .rating {
+    color: yellow;
+  }
+`;
+
 const Lobby = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const login = () => setIsLoggedIn(true);
@@ -111,6 +165,140 @@ const Lobby = () => {
             }
           </HeaderMenu>
         </Header>
+        <Contents>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_8_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_9_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_10_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_12_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_13_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_14_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_8_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_9_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_10_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_12_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_13_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+          <Card>
+            <CardImage>
+              <img src="../static/images/beer/b_14_simg.png" />
+            </CardImage>
+            <CardText>
+              <ul>
+                <li className="beer_name">Gangseo Ale</li>
+                <li>Rating <span className="rating">100</span></li>
+              </ul>
+            </CardText>
+          </Card>
+        </Contents>
       </Body>
     </FullSizeDiv>
   );
